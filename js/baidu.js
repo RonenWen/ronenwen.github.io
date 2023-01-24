0751012b28bbc7fde56be60c0a766b0351d95563
RonenWen <wjqronen@gmail.com>
2023-01-24 15:44:32 +0800

    add baidutongji.js

---
+++ js/baidu.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?d43d4c94da4451acdf5fe5f1a26129e5";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();

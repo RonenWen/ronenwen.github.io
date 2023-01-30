1f67c9ab3c4edac533c571702df8d3070dc55cf0
RonenWen <wjqronen@gmail.com>
2023-01-30 23:59:05 +0800

    update baidujs

--- js/baidu.js
+++ js/baidu.js
@@ -1,7 +1,9 @@
+
 var _hmt = _hmt || [];
 (function() {
   var hm = document.createElement("script");
-  hm.src = "https://hm.baidu.com/hm.js?d43d4c94da4451acdf5fe5f1a26129e5";
+  hm.src = "https://hm.baidu.com/hm.js?7d18356c771017dbe92b21c7e2e08481";
   var s = document.getElementsByTagName("script")[0]; 
   s.parentNode.insertBefore(hm, s);
 })();
+
